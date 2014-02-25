de9946dc31fc4bd56a7cd31ee5c30b654905dc6e
Justin (Zihao) Zhang <Main@wl-10-190-53-194.wireless.duke.edu>
2014-02-24 20:12:14 -0500

    rm bk command

---
+++ src/backEnd/FDBKCommand.java
@@ -0,0 +1,20 @@
+package backEnd;
+
+import java.util.List;
+import frontEnd.Turtle;
+
+public class FDBKCommand extends ActionCommand {
+	
+	protected boolean moveForward;
+	
+	public FDBKCommand(String string, double pixels, List<Turtle> turtles) {
+		super(string, pixels, turtles);
+		if(myString.startsWith("f")){ moveForward = true; }
+		moveForward = false;
+	}
+
+	@Override
+	protected void moveTurtle(Turtle turtle) {
+		//current.setTarget();
+	}
+}

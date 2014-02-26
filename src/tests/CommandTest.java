26e6b645e064fb7c070cf7c29a59aa8238374beb
thedanzhang <dyz2@duke.edu>
2014-02-26 03:51:19 -0500

    Parser can check for numbers aka parameters for commands

--- src/tests/CommandTest.java
+++ src/tests/CommandTest.java
@@ -1,14 +1,26 @@
 package tests;
 
 import static org.junit.Assert.*;
+import java.util.*;
 
 import org.junit.Test;
 
-public class CommandTest {
+import commands.AbstractCommand;
+import commands.FDBKCommand;
+import frontEnd.Turtle;
 
+public class CommandTest {
+	
+	List<Turtle> myTurtles = new ArrayList<Turtle>();
+
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testFDBKCommand() {
-
+		AbstractCommand fd = new FDBKCommand("FD", 50, myTurtles);
+		AbstractCommand bk = new FDBKCommand("BK", 50, myTurtles);
+		assertEquals(50, fd.execute());
+		assertEquals(50, bk.execute());
+		
 	}
 
 }

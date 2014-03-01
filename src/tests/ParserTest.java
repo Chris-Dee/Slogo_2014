c8eaf23494b4a1ee5ca8e78014580e2ae3fc0c77
thedanzhang <dyz2@duke.edu>
2014-02-28 23:22:27 -0500

    Changed text parser and parser test

--- src/tests/ParserTest.java
+++ src/tests/ParserTest.java
@@ -22,4 +22,27 @@ public class ParserTest {
 		assertEquals("50", root.getChildren().get(0).getChildren().get(0).getChildren().get(0).getCommandString());
 	}
 	
+	@Test
+	public void testTwoParameters() {
+		AbstractParser parser = new TextParser();
+		StringNode root = parser.parse("sum fd 50 bk 30");
+		parser.printTree(root);
+		assertEquals("SUM", root.getCommandString());
+		assertEquals("FD", root.getChildren().get(0).getCommandString());
+		assertEquals("50", root.getChildren().get(0).getChildren().get(0).getCommandString());
+		assertEquals("BK", root.getChildren().get(1).getCommandString());
+		assertEquals("30", root.getChildren().get(1).getChildren().get(0).getCommandString());
+
+	}
+	
+	@Test
+	public void testBrackets() {
+		AbstractParser parser = new TextParser();
+		StringNode root = parser.parse("repeat 3 [fd 50 repeat 5 [fd 50]] fd 50");
+		assertEquals("REPEAT", root.getCommandString());
+		assertEquals("3", root.getChildren().get(0).getCommandString());
+		assertEquals("FD 50 REPEAT 5 [FD 50]", root.getChildren().get(1).getCommandString());
+		assertEquals("FD", root.getChildren().get(1).getChildren().get(0).getCommandString());
+	}
+	
 }

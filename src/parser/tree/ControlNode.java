7a55c09c281dab7fdb1c761a770bbc5c6b31350e
thedanzhang <dyz2@duke.edu>
2014-03-01 00:33:02 -0500

    added control and if else ndoes

--- src/parser/tree/ControlNode.java
+++ src/parser/tree/ControlNode.java
@@ -2,14 +2,14 @@ package parser.tree;
 
 public class ControlNode extends StringNode{
 	
-	private String myExpression;
-	private String myCommands;
-	private String myCommands2;
+	protected String myExpression;
+	protected String myCommands;
 
 	public ControlNode(String data, String expression, String commands) {
 		super(data);
 		// TODO Auto-generated constructor stub
-		
+		myExpression = expression;
+		myCommands = commands;
 	}
 
 }
---
+++ src/parser/tree/IfElseNode.java
@@ -0,0 +1,14 @@
+package parser.tree;
+
+public class IfElseNode extends ControlNode{
+	
+	private String myElseCommands;
+
+	public IfElseNode(String data, String expression, String commands,
+			String elseCommands) {
+		super(data, expression, commands);
+		// TODO Auto-generated constructor stub
+		myElseCommands = elseCommands;
+	}
+
+}

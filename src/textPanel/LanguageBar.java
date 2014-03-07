94d1b5f34c22dc9c97731819d3703670f6f6d20a
Chris-Dee <cmd45@duke.edu>
2014-03-07 18:22:12 -0500

    fixed error meesage

--- src/textPanel/LanguageBar.java
+++ src/textPanel/LanguageBar.java
@@ -13,11 +13,11 @@ import backEnd.Managers.LanguageManager;
 
 public class LanguageBar extends JPanel {
 	LanguageManager languageManager;
-public LanguageBar(LanguageManager langManage){
-	super();
-	languageManager=langManage;
-	makeLangList(this);
-}
+	public LanguageBar(LanguageManager langManage){
+		super();
+		languageManager=langManage;
+		makeLangList(this);
+	}
 	private void makeLangList(JPanel homePanel){
 		final JComboBox languagesList=new JComboBox((removeFileNames(new File("src/resources").list())));
 		homePanel.add(languagesList);
@@ -26,7 +26,6 @@ public LanguageBar(LanguageManager langManage){
 			public void actionPerformed(ActionEvent e)
 			{       
 				languageManager.setLanguage((String) languagesList.getSelectedItem());  
-				System.out.println(languageManager.getLanguage());
 			}
 		});  
 	}
@@ -34,9 +33,9 @@ public LanguageBar(LanguageManager langManage){
 		List<String> langList=new ArrayList<String>();
 		for(int i=0;i<s.length;i++){
 			if(s[i].contains(".properties")){
-			String[] str=s[i].split(".properties");
-			System.out.println(str[0]);
-			langList.add(str[0]);
+				String[] str=s[i].split(".properties");
+				System.out.println(str[0]);
+				langList.add(str[0]);
 			}
 		}
 		return langList.toArray();

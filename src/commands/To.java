4f1066e4f4b0fcad6678379cf6d3cf801fa3eff2
Justin (Zihao) Zhang <Main@wl-10-190-4-221.wireless.duke.edu>
2014-03-07 03:21:53 -0500

    To Done

--- src/commands/To.java
+++ src/commands/To.java
@@ -1,5 +1,6 @@
 package commands;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import parser.AbstractParser;
@@ -27,21 +28,29 @@ public class To extends ControlCommand {
 
 	@Override
 	public double execute() throws IllegalCommandException, IllegalParameterException {
-		if(myName == null) return 0;
-		if(myExpression == null) return 0;
-		if(myCommands == null) return 0;
+		if(myName == null || myExpression == null || myCommands == null) return 0;
 		
 		List<StringNode> exprRoots = myParser.parse(myExpression);
 		if(!ifLegalParameter(exprRoots)) return 0;
-		
 		List<StringNode> commandRoots = myParser.parse(myCommands);
-		myParser.ifLegal(commandRoots);
+		if(myParser.hasErrors(commandRoots);
 		
+		List<String> cmdVariables = getVariableListFromListNode(commandRoots);
+		List<String> paraVariables = getVariableListFromListNode(exprRoots);
+		if(!ifTwoStringListsEqual(cmdVariables, paraVariables)) return 0;
+		System.out.println("TO: is Legal");
 		UserCommand cmd = new UserCommand();
 		cmd.setExpression(myExpression);
 		cmd.setCommands(myCommands);
-		myCommandManager.createNewUserCommand(myName, cmd);
+		myCommandManager.createNewUserCommand(myName, exprRoots.size(), cmd);
 		return 1;
+	}
+	
+	protected boolean ifTwoStringListsEqual(List<String> a, List<String> b){
+		for(String s: a){
+			if(!b.contains(s)) return false;
+		}
+		return true;
 	}
 	
 	protected boolean ifLegalParameter(List<StringNode> roots){
@@ -51,6 +60,35 @@ public class To extends ControlCommand {
 			}
 		}
 		return true;
+	}
+	
+	protected List<String> getVariableListFromListNode(List<StringNode> roots){
+		List<String> vList = new ArrayList<String>();
+		for(StringNode root: roots){
+			vList = appendTwoLists(vList, getVariables(root));
+		}
+		return vList;
+	}
+	
+	protected List<String> getVariables(StringNode current){
+		if(current == null) return null;
+		List<String> variables = new ArrayList<String>();
+		if(myParser.isVariable(current.getCommandString())){
+			variables.add(current.getCommandString());
+		}
+		if(current.getChildren() != null){
+			for(StringNode child: current.getChildren()){
+				variables = appendTwoLists(variables, getVariables(child));
+			}
+		}
+		return variables;
+	}
+	
+	protected List<String> appendTwoLists(List<String> a, List<String> b){
+		for(String s: b){
+			a.add(s);
+		}
+		return a;
 	}
 
 }

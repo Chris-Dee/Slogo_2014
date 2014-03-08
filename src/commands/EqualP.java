db193234927e83e32e0f3e75a164963ecd28be77
Justin (Zihao) Zhang <Main@Justins-MacBook-Pro-2.local>
2014-03-08 07:47:56 -0500

    update

--- src/commands/EqualP.java
+++ src/commands/EqualP.java
@@ -6,6 +6,7 @@ public class EqualP extends TwoParameterOperationCommand{
 	
 	@Override
 	public double execute() {
+		System.out.println("EqualP execute called: "+myExpression1+" "+myExpression2);
 		if (myExpression1 == myExpression2) return 1;
 		return 0;
 	}

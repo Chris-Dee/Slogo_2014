eeb716138bd703ae848ee6a7909548bc4778eba3
Justin (Zihao) Zhang <Main@Justins-MacBook-Pro-2.local>
2014-03-08 07:15:58 -0500

    fix hider and show turtle

--- src/commands/HideTurtle.java
+++ src/commands/HideTurtle.java
@@ -5,14 +5,12 @@ import TurtleStuff.Turtle;
 
 public class HideTurtle extends NonParameterTurtleCommand{
 	ImageManager images = new ImageManager();
-	public static final int INVISIBLE_INDEX = 0;
 	
 	public HideTurtle(){} 
 	
 	@Override
 	protected double executeTurtle(Turtle turtle) {
-		turtle.setImage(null);
-		turtle.setImageID(INVISIBLE_INDEX);
+		turtle.hideTurtle();
 		return 0;
 	}
 }

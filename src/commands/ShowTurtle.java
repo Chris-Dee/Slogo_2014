844a0cd0fb3fc434beecbbc957fd7df00050252c
Justin (Zihao) Zhang <Main@Justins-MacBook-Pro-2.local>
2014-03-08 01:54:40 -0500

    update

--- src/commands/ShowTurtle.java
+++ src/commands/ShowTurtle.java
@@ -8,7 +8,7 @@ public class ShowTurtle extends NonParameterTurtleCommand{
 	
 	@Override
 	protected double executeTurtle(Turtle turtle) {
-		turtle.resume();
+		turtle.getImageID()
 		return 1;
 	}
 }

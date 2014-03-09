e4c92729457f5ffe4bae9339ba648f4de88b1b73
Justin (Zihao) Zhang <Main@Justins-MacBook-Pro-2.local>
2014-03-08 23:04:15 -0800

    comment or eliminate unecessary print statements

--- src/commands/For.java
+++ src/commands/For.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-import parser.AbstractParser;
 import parser.TextParser;
 import parser.tree.StringNode;
 import backEnd.Managers.LanguageManager;
@@ -27,36 +26,20 @@ public class For extends ControlCommand {
 	public double execute() throws IllegalCommandException, IllegalParameterException, UndefinedVariableException {
 		TextParser parser = new TextParser();
 		parser.setUserCommandManager(myUserCommandManager);
-		System.out.println();
-		System.out.println("For execute called: "+ myExpression + " " + myCommands);
 		myForFactory.setVariableManager(myLocalVariableManager);
 		myForFactory.setUserCommandManager(myUserCommandManager);
 		Map<String, Double> lastVCopy = getCopyOfMapFromVariableManager(myVariableManager);
 		List<StringNode> expr = parser.parse(myExpression);
 		myForFactory.runCommands(expr, myTurtles);
-		// Pay attention here
-		System.out.println();
-		System.out.println("Pre expression: "+expr.size());
-		System.out.println();
-//		AbstractParser.printListNodes(expr); 
 		myLocalVariableManager.setValueToVariable(":A", 1);
 		List<StringNode> commands = myParser.parse(myCommands);
-//		AbstractParser.printListNodes(commands); 
-		System.out.println();
-		System.out.println("Post expression: "+expr.size());
-		System.out.println();
-		System.out.println("commands: "+expr.get(0).getCommandString());
 		String variable = expr.get(0).getCommandString();
 		System.out.println("variable: " + variable);
 		expr.remove(0);
-//		AbstractParser.printListNodes(expr);
-		System.out.println("expr start: " + expr.get(0).getCommandString());
 		double answer = 0;
 		try{
 			answer = myForFactory.runForLoopCommands(expr, commands, variable, myTurtles);
-		} catch (Exception e){
-			e.printStackTrace();
-		}
+		} catch (Exception e){ throw new IllegalCommandException(); }
 		backToLastVariableSpace(lastVCopy);
 		return answer;
 	}
